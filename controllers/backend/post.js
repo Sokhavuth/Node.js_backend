022c4127eb0b1a7e5a9fcb734e60716387a74be7
Sokhavuth <vuthdevelop@gmail.com>
2023-02-08 11:24:51 +0700

    Make it better

--- controllers/backend/post.js
+++ controllers/backend/post.js
@@ -1,11 +1,16 @@
 //controllers/backend/post.js
 
+import category from '../../models/category.js'
+
 class Post{
     async getPage(req,res){
         req.settings.pageTitle = 'ទំព័រ​ការផ្សាយ'
         req.settings.message = ''
         req.settings.route = '/admin/post'
-  
+        req.settings.type = 'post'
+
+        req.settings.categories = await category.getItem(req,'all')
+        
         res.render('base', {data:req.settings})
     }
 }
--- public/scripts/backend/video.js
+++ public/scripts/backend/video.js
@@ -1,20 +1,30 @@
-var episode = 0
+// static/scripts/video.js
+
+let episode = 0
 
 const genJson = () => {
+    let json = $('input[name="videos"]').val()
+    if((json !== '')&&(json !== '[]')){
+        json = JSON.parse(json)
+        episode = json.length
+    }else{
+        episode = 0
+    }
+
     const type = $('select[name="type"').val()
-    const id = $('input[name="id"').val()
-    const ending = $('select[name="ending"').val()
+    const id = $('input[name="videoid"').val()
+    const status = $('select[name="status"').val()
             
-    var entries = {
+    let video = {
         type: type,
         id: id,
-        ending: ending,
+        status: status,
     }
         
-    var success = false
+    let success = false
     
-    for(let v in entries){
-        if(entries[v] === ''){
+    for(let v in video){
+        if(video[v] === ''){
             alert('You need to fill the required field '+v)
             success = false
             break
@@ -24,38 +34,66 @@ const genJson = () => {
     }
 
     if(success){
-        let json = $('input[name="entries"]').val()
-        entries = {
+        let json = $('input[name="videos"]').val()
+        video = {
             type: type,
             id: id,
-            ending: ending,
+            status: status,
         }
         if((json === '')){
-            json = JSON.stringify([entries])
-            $('input[name="entries"]').val(json)
+            json = JSON.stringify([video])
+            $('input[name="videos"]').val(json)
         }else{
             json = JSON.parse(json)
-            json.push(entries)
+            json.push(video)
             json = JSON.stringify(json)
-            $('input[name="entries"').val(json)
+            $('input[name="videos"').val(json)
         }
 
-        let html = `<td title="Delete" onClick="deleteRow(event)" class="episode">${++episode}</td>`
-        for(let v in entries){
-            html += `<td class="td${episode}">${entries[v]}</td>`
+        let html =``
+
+        for(let v in video){
+            html += `<input value="${video[v]}" required />`
         }
 
-        if($('.viddata').html() === ''){
-            $('.viddata').append('<tr>')
-            $('.viddata').append('<th>ភាគ/លុប</th>')
-            $('.viddata').append('<th>ប្រភេទ​</th>')
-            $('.viddata').append('<th>អត្តសញ្ញាណ​</th>')
-            $('.viddata').append('<th>ចប់ឬ​នៅ?</th>')
-            $('.viddata').append('</tr>')
+        html += `<p title="Delete" onClick="deleteRow(event)" class="episode">${++episode}</p>`
+        html = `<div>${html}</div>`
+        
+        if($('.viddata div').html() === ''){
+            $('.viddata div').append('<b>Video type​</b>')
+            $('.viddata div').append('<b>Video id​</b>')
+            $('.viddata div').append('<b>Status</b>')
+            $('.viddata div').append('<b>Part/Delete</b>')
         }
-
-        $('.viddata').append(`<tr>${html}</tr>`)
+        
+        $('.viddata div:eq(0)' ).after(html)
     }
+}
+
+function submitForm(e){
+    e.preventDefault()
+    
+    const is_video = $('input[name="videos"').val()
+
+    if((is_video !== '') && (is_video !== '[]')){
+        episode = JSON.parse(is_video).length
+        let videos = []
+        let part = {}
+        let key = {0:'type', 1:'id', 2:'status'}
+
+        for(let v=1; v<=episode; v++){
+            for(let j=0; j<3; j++){
+                part[key[j]] = $(`.viddata div:eq(${v}) input:eq(${j})`).val()
+            }
+
+            videos.push({...part})
+        }
+    
+        const json = JSON.stringify(videos)
+        $('input[name="videos"').val(json)
+    }
+    
+    document.forms["form"].submit()
 }
 
 function deleteRow(e) {
@@ -63,14 +101,14 @@ function deleteRow(e) {
     
     let index = parseInt(e.target.innerHTML)
     index = index - 1
-    let json = $('input[name="entries"]').val()
+    let json = $('input[name="videos"]').val()
     json = JSON.parse(json)
     json.splice(index, 1)
+    episode = json.length
     json = JSON.stringify(json)
-    $('input[name="entries"').val(json)
+    $('input[name="videos"').val(json)
 
-    episode -= 1
-    for(let v=0; v<episode; v++){
-        $('.episode').eq(v).html(v+1)
+    for(let v=episode; v>-1; v--){
+        $('.episode').eq(v).html(episode-v)
     }
 }
